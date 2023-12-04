08cc904cc197a07d76a168b049a82c56c0187fb1
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-12-04 17:15:33 -0500

    Initial

---
+++ LeapPlugin.R
@@ -0,0 +1,49 @@
+library(AppliedPredictiveModeling)
+transparentTheme(trans = .4)
+library(caret)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+
+input <- function(inputfile) {
+	pfix = prefix()
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+   # Need to get the three files
+   csvfile <<- paste(pfix, parameters["csvfile", 2], sep="/")
+
+   training <<- read.csv(csvfile)
+   fitControl <<- readRDS(paste(pfix, parameters["fitControl", 2], sep="/"))
+   myType <<- "Regular"
+  if ("type" %in% rownames(parameters)) {
+  myType  <<- as.integer(parameters["type", 2])
+  }
+
+
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+set.seed(825)
+if (myType == "forward")
+{
+result <- train(V1 ~ ., data = training, 
+                 method = "leapForward", 
+                 trControl = fitControl)
+
+}
+else if (myType == "backward") {
+result <- train(V1 ~ ., data = training, 
+                 method = "leapBackward", 
+                 trControl = fitControl)
+}
+	else {
+result <- train(V1 ~ ., data = training, 
+                 method = "leapSeq", 
+                 trControl = fitControl)
+}
+print(result)
+saveRDS(result, outputfile)
+}
